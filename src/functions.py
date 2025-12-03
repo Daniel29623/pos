38e68814ccec5a18b6e4e6de92838491ef0dccf2
Daniel29623 <varga.dani.29623@gmail.com>
2025-12-03 10:34:37 +0100

    Added error handling

--- src/functions.py
+++ src/functions.py
@@ -1,5 +1,5 @@
 from json import load, dumps
-from os import name, system
+from os import name, system, listdir, remove
 
 def clear():
     if name == "nt":
@@ -15,7 +15,7 @@ def pause():
 
 def create(path_name: str) -> dict:
     with open("data/" + path_name + ".json", "w") as json:
-        json.write(dumps({}))
+        json.write(dumps({}) + "\n")
     with open("data/" + path_name + ".json", "r") as json:
         data = load(json)
     return data
@@ -26,3 +26,7 @@ def read(path_name: str) -> dict:
 def write(path_name: str, data: dict) -> None:
     with open("data/" + path_name + ".json", "w") as json:
         json.write(dumps(data) + "\n")
+
+def rmAll(path: str) -> None:
+    for file in listdir("data/" + path):
+        remove("data/" + path + "/" + file)
--- src/registers.py
+++ src/registers.py
@@ -1,35 +1,65 @@
 from getpass import getpass
 from os import mkdir, rmdir, remove as rm
-import functions as f
+from src import functions as f
 
 class reg:
     def create() -> None:
         registers = f.read("registers")
+        registers = registers["registers"]
         while True:
-            cn = input("What would be the codename of your register? (max 10 characters) ")
-            if len(list(cn)) <= 10:
+            running = True
+            while running:
+                cn = input("What would be the codename of your register? (max 10 characters) ")
+                if len(list(cn)) <= 10 and len(list(cn)) > 0:
+                    for char in list(cn):
+                        if char == " ":
+                            print("Codename can't contain spaces!")
+                            f.pause()
+                        else:
+                            running = False
+                            break
+                else:
+                    print("The codename must be more than 0 characters and not more then 10!")
+                    f.pause()
+            try:
+                mkdir("data/" + cn)
                 break
-        mkdir("data/" + cn)
+            except FileExistsError:
+                print("A register with this codename already exists!")
+                f.pause()
         regData = f.create(cn + "/data")
-        regData["name"] = input("What will be the name of your register? ")
-        while True:
-            pw = getpass("New password: ")
-            pwa = getpass("Password again: ")
-            if pw == pwa:
-                regData["pw"] = pw
-                break
-        while True:
-            acc = input("Will you be using accounts in this register? (y/n) ")
-            if acc.lower().startswith("y"):
-                regData["acc"] = True
-                while True:
-                    regData["accs"] = [input("What will be the name of the first account? ")]
-                    if regData["accs"][0] != "main":
+        try:
+            regData["name"] = input("What will be the name of your register? ")
+            while True:
+                pw = getpass("New password: ")
+                pwa = getpass("Password again: ")
+                if pw == pwa:
+                    regData["pw"] = pw
+                    break
+                else:
+                    print("Password's don't match!")
+                    f.pause()
+            while True:
+                acc = input("Will you be using accounts in this register? (y/n) ")
+                if acc.lower().startswith("y"):
+                    regData["acc"] = True
+                    while True:
+                        regData["accs"] = [input("What will be the name of the first account? ")]
                         break
-                f.create(cn + "/accs/" + regData["accs"][0])
-                mkdir("data/" + cn + "/accs")
-                break
-            elif acc.lower().startswith("n"):
-                regData["acc"] = False
-                f.create(cn + "/main")
-                break
+                    mkdir("data/" + cn + "/accs")
+                    f.create(cn + "/accs/" + regData["accs"][0])
+                    break
+                elif acc.lower().startswith("n"):
+                    regData["acc"] = False
+                    f.create(cn + "/main")
+                    break
+                else:
+                    print("Please enter a valid answer!")
+                    f.pause()
+            registers.append(cn)
+            registers = {"registers": registers}
+            f.write("registers", registers)
+            f.write(cn + "/data", regData)
+        except KeyboardInterrupt:
+            f.rmAll(cn)
+            rmdir("data/" + cn)
